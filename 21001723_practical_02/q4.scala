646c189ab018f8eb6a53a10bcab31e71e741b39c
NirmalHettiarachchi <nirmalhettiarachchi5@gmail.com>
2023-06-19 22:27:56 +0530

    Edited q4.scala code

--- 21001723_practical_02/q4.scala
+++ 21001723_practical_02/q4.scala
@@ -6,15 +6,22 @@ object Theater {
   }
 
   def findBestTicketPrice(performanceCost: Int, attendeeCost: Int): Int = {
-    val ticketPrices = (-3 to 6).map(getTicketPrice) //because the lowest ticket price is 0 (15 - 5*3) and the lowest number of attendaces is 0 (120 - 20*6)
-    val numAttendances = (-3 to 6).map(getNumOfAttendance)
-    val profits = ticketPrices.zip(numAttendances).map {
-      case (ticketPrice, numOfAttendance) =>
-        (ticketPrice * numOfAttendance) - performanceCost - (attendeeCost * numOfAttendance)
-    }
+    val ticketPrices = getTicketPrices()
+    val numOfAttendances = getNumOfAttendances()
+    val profits = getProfits(ticketPrices, numOfAttendances, performanceCost, attendeeCost)
+
     val bestTicketPrice = ticketPrices(profits.indexOf(profits.max))  //get the index of the max profit from the profits and use it to retrieve the corresponding ticket price
     bestTicketPrice    
   }
+
+  def getTicketPrices(): Seq[Int] =  (-3 to 6).map(getTicketPrice)  //because the lowest ticket price is 0 (15 - 5*3) and the lowest number of attendaces is 0 (120 - 20*6)
+
+  def getNumOfAttendances(): Seq[Int] = (-3 to 6).map(getNumOfAttendance) //because the lowest ticket price is 0 (15 - 5*3) and the lowest number of attendaces is 0 (120 - 20*6)
+
+  def getProfits(ticketPrices: Seq[Int], numOfAttendances: Seq[Int], performanceCost: Int, attendeeCost: Int): Seq[Int] = ticketPrices.zip(numOfAttendances).map {
+      case (ticketPrice, numOfAttendance) =>
+        (ticketPrice * numOfAttendance) - performanceCost - (attendeeCost * numOfAttendance)
+    }
 
   def getTicketPrice(numOfMultipliesBy5: Int): Int = 15 + (numOfMultipliesBy5 * 5)
 
