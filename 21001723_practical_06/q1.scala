e51cd4128482465c1d31b61b30942e543aca3381
NirmalHettiarachchi <nirmalhettiarachchi5@gmail.com>
2023-07-28 08:26:57 +0530

    Updated the code to use map

--- 21001723_practical_06/q1.scala
+++ 21001723_practical_06/q1.scala
@@ -1,28 +1,27 @@
 object Main {
 
-  def cipher(str: String, functionName: String=>String): String = {
-    functionName(str)
+  def cipher(str: String, functionName: (String, Integer)=>String, n: Integer): String = {
+    functionName(str, n)
   }
 
-  def encrypt(str: String): String = {
-    var enStr = ""
-    for (char <- str) {
-      val newChar = if (char == 'Z') 'A' else (char.toInt + 1).toChar
-      enStr += newChar
+  def encrypt(str: String, n: Integer): String = {
+    str.map { char =>
+      val temp = char.toInt + n
+      val newChar = if (temp > 'Z'.toInt) (('A'.toInt - 1) + (temp - 'Z'.toInt)).toChar else temp.toChar
+      newChar
     }
-    enStr
   }
 
-  def decrypt(str: String): String = {
-    var deStr = ""
-    for (char <- str) {
-      val newChar = if (char == 'A') 'Z' else (char.toInt - 1).toChar
-      deStr += newChar
+  def decrypt(str: String, n: Integer): String = {
+    str.map { char =>
+      val temp = char.toInt - n
+      val newChar = if (temp < 'A'.toInt) (('Z'.toInt + 1) - ('A'.toInt - temp)).toChar else temp.toChar
+      newChar
     }
-    deStr
   }
 
   def main(args: Array[String]) = {
-    print(cipher("ABC", encrypt))
+    println(cipher("XYZ", encrypt, 2))
+    println(cipher("ZAB", decrypt, 2))
   }
 }
